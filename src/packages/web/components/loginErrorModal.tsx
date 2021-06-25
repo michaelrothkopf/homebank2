b10068b51473f22924e92cb641cb8d97dc4e2156
mrbros35 <michaelhrothkopf@gmail.com>
2021-06-24 20:50:45 -0400

    fixed some eslint annoyances

--- src/packages/web/components/loginErrorModal.tsx
+++ src/packages/web/components/loginErrorModal.tsx
@@ -17,8 +17,8 @@ export default function LoginErrorModal()
             <div className="modal">
                 <div className="modal-background"></div>
                 <div className="modal-content">
-                    <h1 className="title is-1">Warning: You aren't logged in!</h1>
-                    It appears you aren't logged in to homebank! This may cause issues or errors loading data, and you won't be able to use homebank as intended. Click the button below to log in:
+                    <h1 className="title is-1">Warning: You aren&apos;t logged in!</h1>
+                    It appears you aren&apos;t logged in to homebank! This may cause issues or errors loading data, and you won&apos;t be able to use homebank as intended. Click the button below to log in:
                     <a href="/" className="button">Log In</a>
                 </div>
                 <button className="modal-close is-large" aria-label="close"></button>
